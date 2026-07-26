7ca9bc47fdc25845b198b21a142e6ef1982a64cc
Vasanthan Yogeshwaran <yvasanthan007@gmail.com>
2026-07-26 09:44:33 +0530

    Debugged level1

--- desktop-ui/src/renderer/types.d.ts
+++ desktop-ui/src/renderer/types.d.ts
@@ -17,3 +17,48 @@ interface ZyraAPI {
 interface Window {
   zyraAPI: ZyraAPI;
 }
+
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.less' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
